3e0484de9f3c1205142725640a0fcaabef0f3f38
TokaHv <hittinpwithskeet.cc@gmail.com>
2025-11-26 02:35:36 +0800

    Update server.js

--- keepalive/server.js
+++ keepalive/server.js
@@ -4,7 +4,7 @@ const app = express();
 
 // Simple homepage to show bot is running
 app.get("/", (req, res) => {
-  res.send("Bot is running and Render is awake.");
+  res.send("Bot is running and Railway is awake.");
 });
 
 // Optional detailed status endpoint
